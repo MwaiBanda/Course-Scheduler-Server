2f36f55658bc800a097da4d817b2b038ca3f5639
Mwai Banda <bandamwai@gmail.com>
2023-05-04 22:47:14 -0500

    Added cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Redis } from "ioredis";
 import express from "express";
+import cors from "cors"
 
 const client = new Redis("redis://default:655d0d20b9b1449683b4c37d59cd35ae@loyal-sloth-32673.upstash.io:32673")
 
@@ -7,6 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', "*");
+    next();
+});
 
 app.get("/", (req, res) => {
   res.send("<h6>Group 3 backend</h6>")
