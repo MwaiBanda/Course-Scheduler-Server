33f8f89ee8c17673dae848e8e30890b79563b85a
Mwai Banda <bandamwai@gmail.com>
2023-05-04 23:09:13 -0500

    CORs middleware update

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Headers', "*");
     next();
