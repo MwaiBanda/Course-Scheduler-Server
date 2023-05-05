64cea51d738878bb9beff73a04071fc3b6fcf87a
Mwai Banda <bandamwai@gmail.com>
2023-05-04 19:45:38 -0500

    changes courses get response

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { Redis } from "ioredis";
 import express from "express";
-import path from "path";
 
 const client = new Redis("redis://default:655d0d20b9b1449683b4c37d59cd35ae@loyal-sloth-32673.upstash.io:32673")
 
@@ -15,7 +14,7 @@ app.get("/", (req, res) => {
 
 app.get( "/courses", async ( req, res ) => {
     const courses = await client.get("courses") ?? "[]"
-    res.json({ results: JSON.parse(courses) });
+    res.json(courses);
 })
 
 app.post('/courses', async (req, res) => {
