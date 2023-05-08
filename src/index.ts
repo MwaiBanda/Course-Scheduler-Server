fcb89414bb45833e199e3a5931a650ccf9028c6b
Mwai Banda <bandamwai@gmail.com>
2023-05-08 11:31:49 -0500

    Added users post request

--- src/index.ts
+++ src/index.ts
@@ -20,14 +20,23 @@ app.get("/", (req, res) => {
   res.send("<h6>Group 3 backend</h6>")
 })
 
-app.get( "/courses", async ( req, res ) => {
-    const courses = await client.get("courses")
-    res.json(JSON.parse(courses)).sendStatus(200);
-})
-
 app.get( "/users", async ( req, res ) => {
     const users = await client.get("users")
     res.json(JSON.parse(users)).sendStatus(200);
+})
+
+app.post('/users', async (req, res) => {
+    const users = req.body as any[]
+    console.log("[POST]\n" + `${users.map((course) => {
+      return course.name
+    })}`)
+    await client.set("users", JSON.stringify(users))
+    res.json(users).sendStatus(200)
+})
+
+app.get( "/courses", async ( req, res ) => {
+    const courses = await client.get("courses")
+    res.json(JSON.parse(courses)).sendStatus(200);
 })
 
 app.post('/courses', async (req, res) => {
