40f138826515d1e0f3a1f352ab79e87cae941f17
Mwai Banda <bandamwai@gmail.com>
2023-05-08 17:28:37 -0500

    Added error handling

--- src/index.ts
+++ src/index.ts
@@ -21,31 +21,48 @@ app.get("/", (req, res) => {
 })
 
 app.get( "/users", async ( req, res ) => {
+  try {
     const users = await client.get("users")
     res.json(JSON.parse(users)).sendStatus(200);
+  } catch (error) {
+    res.sendStatus(500).json(error.message)
+  }
+    
 })
 
 app.post('/users', async (req, res) => {
+  try {
     const users = req.body as any[]
     console.log("[POST]\n" + `${users.map((course) => {
       return course.name
     })}`)
     await client.set("users", JSON.stringify(users))
     res.json(users).sendStatus(200)
+  } catch (error) {
+    res.sendStatus(500).json(error.message)
+  }
 })
 
 app.get( "/courses", async ( req, res ) => {
+  try {
     const courses = await client.get("courses")
     res.json(JSON.parse(courses)).sendStatus(200);
+  } catch (error) {
+    res.sendStatus(500).json(error.message)
+  }
 })
 
 app.post('/courses', async (req, res) => {
+  try {
     const courses = req.body as any[]
     console.log("[POST]\n" + `${courses.map((course) => {
       return course.name
     })}`)
     await client.set("courses", JSON.stringify(courses))
     res.json(courses).sendStatus(200)
+  } catch (error) {
+    res.sendStatus(500).json(error.message)
+  }
 })
 
 app.listen( PORT, () => {
